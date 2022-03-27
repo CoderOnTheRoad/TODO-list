95d80dcfa9adea11439cfedc70c798907bc66b73
SOURAV GHOSH <sg22945@gmail.com>
2022-03-27 21:00:25 +0530

    inititial setup of express router

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+//search in google about express router for better understanding;
+const express=require("express");
+const router=express.Router();
+console.log("router loaded");
+
+
+
+
+module.exports=router;
