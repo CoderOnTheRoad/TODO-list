5cce1fe6978a408393a7dfca1b8f238fdf83f43c
SOURAV GHOSH <sg22945@gmail.com>
2022-07-15 21:07:24 +0530

    deploy phase 1

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose= require("mongoose");
 //search mongoose in google for the configuration
-mongoose.connect("mongodb://localhost/todoList");//created a database named todo-list and mongobd seever always runs on port 27017
+mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/todoList");//created a database named todo-list and mongobd seever always runs on port 27017
 const db=mongoose.connection; //by this db variable we can access the database
 db.on("error",console.error.bind(console,"error in starting the database")); 
 db.once("open",function(){console.log("successfully connected to the database")});
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express=require("express"); //adding express library 
 const app=express(); //fire up exress
-const port=8000; // port on which our website will run
+const port=process.env.PORT||3000; // port on which our website will run
 const db=require("./config/mongoose");
 const bodyParser=require("body-parser");
 app.use(bodyParser.urlencoded({extended:false}));
